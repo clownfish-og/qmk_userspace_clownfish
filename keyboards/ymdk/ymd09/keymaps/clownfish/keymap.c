5b63f3ee7837928703fc0421a65dbe48392febca
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-06-25 11:10:09 -0400

    update keycodes

--- keyboards/ymdk/ymd09/keymaps/clownfish/keymap.c
+++ keyboards/ymdk/ymd09/keymaps/clownfish/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                TO(2),   KC_BRID, KC_BRIU,
                _______, KC_VOLD, KC_VOLU),
 
-  [2] = LAYOUT(TO(0),   THIS,    KEK,
+  [2] = LAYOUT(TO(0),   CFOG,    KEK,
                DRAGON,  LOVE,    LFISH,
                BUP,     SPAM,    KC_ENT),
 
