cc260f33ef9d2ae9663bd3e99a7efc5ff0ba97da
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-07-14 11:27:28 -0400

    rm duplicate header

--- keyboards/kprepublic/cstc40/rev3/keymaps/vial_clownfish/keymap.c
+++ keyboards/kprepublic/cstc40/rev3/keymaps/vial_clownfish/keymap.c
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "modules/elpekenin/ledmap/elpekenin/ledmap.h"
 #include QMK_KEYBOARD_H
 #include "rgb_keys.h"
 #include "elpekenin/ledmap.h"
